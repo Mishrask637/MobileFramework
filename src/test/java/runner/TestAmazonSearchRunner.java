7d36c05c1e6eebb458bfa24973440385e890575d
Mishrask637 <Mishrask637@gmail.com>
2024-05-10 13:45:53 +0530

    Updated Runner Class Name

---
+++ src/test/java/runner/TestAmazonSearchRunner.java
@@ -0,0 +1,13 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src/test/resources/features/AmazonSearch.feature", glue = { "stepdefinitions",
+		"hooks" }, dryRun = false, plugin = { "pretty", "html:target/reports/html/cucumberReports.html",
+				"json:target/reports/json/cucumberReports.json" }, monochrome = true
+
+)
+public class TestAmazonSearchRunner extends AbstractTestNGCucumberTests {
+}
+
